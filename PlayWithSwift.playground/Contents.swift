30e474a6de0584e381701be13dff089cf57228ac
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-25 08:37:44 -0600

    Played with variables in the playground

--- PlayWithSwift.playground/Contents.swift
+++ PlayWithSwift.playground/Contents.swift
@@ -2,5 +2,19 @@
 
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+var ramen : String
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+
+var myAge = -98
+print(myAge)
+
+var myName = "Tyler"
+print("My name is " + myName)
+
+var acceleration : Double
+let gravity = -9.8
+acceleration = gravity
+print(acceleration)
