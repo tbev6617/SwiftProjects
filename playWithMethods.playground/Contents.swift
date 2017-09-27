01461aa973d6d7710a6081d8c04b1460d6910301
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-27 08:43:27 -0600

    Added methods to my playground

--- playWithMethods.playground/Contents.swift
+++ playWithMethods.playground/Contents.swift
@@ -23,3 +23,44 @@ public func evenCrazier (count : Int,bandName : String,
     print(info)
 }
 evenCrazier(count : 2, bandName : "SpongeBob", date : "yesterday")
+
+public func calculateDays( first firstDay : Int, next nextDay : Int) -> Int
+{
+    let answer = nextDay - firstDay;
+    
+    return answer;
+}
+
+calculateDays(first : 1, next : 4)
+
+public func name(friendName homeName : String) -> Void
+{
+    let info = "At my home my name is \(homeName)"
+    print(info)
+}
+name(friendName : "Dr Fish")
+var guess = 1;
+if (guess == 2)
+{
+    print("Wahoo!")
+}
+else
+{
+    print("Not likely")
+}
+var i = 0
+while (i < 10)
+{
+    print(i + 1)
+    i += 1
+}
+
+
+
+
+
+
+
+
+
+
