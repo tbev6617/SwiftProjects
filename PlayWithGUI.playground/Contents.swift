bbf0b6f2d80e2996c0463bc6d80679d177282882
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-04 09:00:24 -0600

    Made 3 UI labels and another button

--- PlayWithGUI.playground/Contents.swift
+++ PlayWithGUI.playground/Contents.swift
@@ -4,15 +4,28 @@ import UIKit
 
 var str = "Working with GUI"
 
-var firstButton = UIButton(frame : CGRect(x : 50, y : 50, width : 180, height: 100))
+var firstButton = UIButton(frame : CGRect(x : 50, y : 50, width : 180, height : 100))
 firstButton.backgroundColor = .orange
 firstButton.setTitle("Button", for : .normal)
-var temp = firstButton.titleLabel
-firstButton.setTitle("Look a button", for : .normal)
+
+var label1 = firstButton.titleLabel
 
 var secondButton = UIButton(frame: CGRect(x : 50, y : 50, width : 180, height : 100))
 firstButton.backgroundColor = .black
 firstButton.setTitle("New Button", for : .normal)
+
+var label2 = UILabel()
+label2.text = "Newest Button"
+
+var label3 = UILabel(frame : CGRect(x : 50, y : 50, width : 180, height : 100))
+label3.text = "Hooray"
+
+var thirdButton = UIButton(frame : CGRect(x : 50, y : 50, width : 180, height : 100))
+thirdButton.backgroundColor = .red
+thirdButton.setTitle(label2.text, for : .normal)
+
+
+
 
 
 
